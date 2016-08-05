c1603dd00b2ef429bcdb67d3c967e0cbaaadbd01
shoji.fujita <shoji.fujita.0819@gmail.com>
2016-08-06 02:31:32 +0900

    Initial Commit

---
+++ Utility/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Utility
+//
+//  Created by 藤田勝司 on 2016/08/06.
+//  Copyright © 2016年 藤田勝司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
